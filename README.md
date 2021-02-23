5b8101499bf146f21786a709586b6fe8e1de46a1
Daniel Ruiz de Alegría <daniel@drasite.com>
2021-02-22 23:09:24 -0300

    .

---
+++ README.md
@@ -0,0 +1,18 @@
+# fies
+
+Armazenando dados do Fundo de Financiamento ao Estudante do Ensino Superior (FIES) por meio de um arquivo CSV, utilizando o banco de dados PostgreSQL.
+
+1) Fazer download do arquivo CSV dos dados do FIES disponibilizado na URL abaixo:
+https://drive.google.com/file/d/14GqIHS9uMn7iayKm_RIK_HglDrY5UMLd/view?usp=sharing
+
+2) Tabela no Banco de Dados PostgreSQL para armazenar os dados do arquivo CSV obtido na atividade 1.
+
+3) Importando os dados do arquivo CSV obtido na atividade 1 para a tabela criada na atividade 2.
+
+4) Os dez maiores valores de mensalidades dos financiamentos concedidos em ordem decrescente para a região nordeste no mês de setembro de 2019. Além da mensalidade, devem ser apresentados os respectivos cursos, instituição de ensino superior, estado, município.
+
+5) Os dez instituições de ensino superior da região nordeste no mês de setembro de 2019 com maior número de alunos contemplados com o financiamento. Além das instituições de ensino superior e a quantidade de alunos contemplados, devem ser apresentados os respectivos estados e municípios das instituições de ensino superior.
+
+6) Os dez cursos da região nordeste com maior número de alunos contemplados com o financiamento no mês de setembro de 2019. Além dos cursos e da quantidade de alunos contemplados, devem ser apresentados os respectivos estados e municípios.
+
+7) Código em formato Jupyter Notebook para cada uma das consultas das atividades 4, 5 e 6, foi utilizada a biblioteca Plotly com Python para apresentar gráficos Scatter (https://plotly.com/python/line-and-scatter/) usando cores e os eixos (X e Y).
---
+++ atividades_2-3-4-5-6.sql
@@ -0,0 +1,74 @@
+/********** ATIVIDADE 2 **********/
+CREATE TABLE fies_csv (
+	est CHAR(3), 
+	mun VARCHAR(30), 
+	cod_mant VARCHAR(10), 
+	cnpj_mant VARCHAR(20), 
+	mant VARCHAR(150), 
+	mun_mant VARCHAR(20), 
+	cod_ies VARCHAR(10),
+	no_ies VARCHAR(100),
+	mun_ies VARCHAR(20),
+	proc INTEGER,
+	contrato INTEGER,
+	cod_adit VARCHAR(10),
+	cod_agfin VARCHAR(10),
+	agfin VARCHAR(50),
+	sg_agfin CHAR(4),
+	nu_mes INTEGER, 
+	nu_sem INTEGER, 
+	nu_ano INTEGER, 
+	vl_mens REAL, 
+	insc INTEGER, 
+	tp_fianca CHAR(2), 
+	ano_insc CHAR(10), 
+	p_prouni CHAR(5), 
+	p_solicit CHAR(7), 
+	vl_repass CHAR(10), 
+	dt_nasc DATE, 
+	st_def CHAR(2), 
+	st_escola CHAR(2), 
+	cod_cidade VARCHAR(10), 
+	sg_sexo CHAR(2), 
+	sexo VARCHAR(20), 
+	cod_estcivil VARCHAR(10), 
+	est_civil VARCHAR(20), 
+	sg_raca CHAR(2), 
+	raca VARCHAR(10), 
+	insc_ext INTEGER, 
+	contrato_ext INTEGER, 
+	cod_ies_ext VARCHAR(10), 
+	ies_ext VARCHAR(100), 
+	cod_campus VARCHAR(10), 
+	campus VARCHAR(100), 
+	p_finan INTEGER, 
+	vlmens_ext REAL, 
+	qtsem_fin VARCHAR(10), 
+	cod_adit_ext VARCHAR(10), 
+	cod_curso VARCHAR(10), 
+	curso VARCHAR(100), 
+	cod_tpcurso VARCHAR(10), 
+	tipo_curso VARCHAR(50), 
+	cod_periodic VARCHAR(10), 
+	periodic VARCHAR(50), 
+	vl_semestre CHAR(10), 
+	ano_proc INTEGER, 
+	mes_proc INTEGER
+)
+
+/********** ATIVIDADE 3 **********/
+COPY fies_csv (est, mun, cod_mant, cnpj_mant, mant, mun_mant, cod_ies, no_ies, mun_ies, proc, contrato, cod_adit, cod_agfin, agfin, sg_agfin, nu_mes, nu_sem, nu_ano, vl_mens, insc, tp_fianca, ano_insc, p_prouni, p_solicit, vl_repass, dt_nasc, st_def, st_escola, cod_cidade, sg_sexo, sexo, cod_estcivil, est_civil, sg_raca, raca, insc_ext, contrato_ext, cod_ies_ext, ies_ext, cod_campus, campus, p_finan, vlmens_ext, qtsem_fin, cod_adit_ext, cod_curso, curso, cod_tpcurso, tipo_curso, cod_periodic, periodic, vl_semestre, ano_proc, mes_proc)
+FROM '/home/lucas/bkp_tb_fies_v3.csv'
+DELIMITER ','
+CSV HEADER;
+
+/********** ATIVIDADE 4 **********/
+SELECT vl_mens, curso, no_ies, est, mun FROM fies_csv WHERE (est='AL' OR est='BA' OR est='PB' OR est='PE' OR est='RN' OR est='CE' OR est='SE' OR est='MA' OR est='PI') AND nu_mes=09 AND nu_ano=2019 ORDER BY vl_mens DESC LIMIT 10
+
+
+/********** ATIVIDADE 5 **********/
+SELECT DISTINCT no_ies, p_solicit, est, mun_ies FROM fies_csv WHERE (est='AL' OR est='BA' OR est='PB' OR est='PE' OR est='RN' OR est='CE' OR est='SE' OR est='MA' OR est='PI') AND nu_mes=09 AND nu_ano=2019 ORDER BY p_solicit DESC LIMIT 10
+
+/********** ATIVIDADE 6 **********/
+SELECT curso, p_solicit, est, mun_ies FROM fies_csv WHERE (est='AL' OR est='BA' OR est='PB' OR est='PE' OR est='RN' OR est='CE' OR est='SE' OR est='MA' OR est='PI') AND nu_mes=09 AND nu_ano=2019 ORDER BY p_solicit DESC LIMIT 10
+
